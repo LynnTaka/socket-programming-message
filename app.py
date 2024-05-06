10c9cff7a5fa7fc3b7ba9ef4a1fcd02b01399866
Kai Xue <98782410+kaicx1@users.noreply.github.com>
2024-05-06 15:36:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ def disconnect():
     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True)
